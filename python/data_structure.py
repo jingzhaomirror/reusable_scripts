6e8a8df967f214e846890465b86a6ab8857949c2
mirror <39384094+jingzhaomirror@users.noreply.github.com>
2019-01-20 21:39:53 -0800

    Update data_structure.py

--- python/data_structure.py
+++ python/data_structure.py
@@ -1,4 +1,4 @@
-# stack implementation as python list, where the top of the stack is the end of the list; supported operations: isEmpty(), push(item), pop(), peek(), size()
+# Stack implementation as python list, where the top of the stack is the end of the list; supported operations: isEmpty(), push(item), pop(), peek(), size()
 class Stack:
      def __init__(self):
          self.items = []
@@ -20,4 +20,21 @@ class Stack:
 
 # ---------------
 
-# 
+# Queue implementation as python list, where the front of the queue (item to be removed) is the end of the list;supported operations: isEmpty(), push(item), pop(), peek(), size()
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.insert(0,item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+     
+     
