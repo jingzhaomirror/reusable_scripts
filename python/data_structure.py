a77cbf4cd72ebea57ca4ab503d3312562570163c
mirror <39384094+jingzhaomirror@users.noreply.github.com>
2019-01-28 16:21:09 -0800

    Update data_structure.py

--- python/data_structure.py
+++ python/data_structure.py
@@ -20,7 +20,7 @@ class Stack:
 
 # ---------------
 
-# Queue implementation as python list, where the front of the queue (item to be removed) is the end of the list;supported operations: isEmpty(), push(item), pop(), peek(), size()
+# Queue implementation as python list, where the front of the queue (item to be removed) is the end of the list;supported operations: isEmpty(), enqueue(item), dequeue(), size()
 class Queue:
     def __init__(self):
         self.items = []
@@ -37,4 +37,27 @@ class Queue:
     def size(self):
         return len(self.items)
      
-     
+# ----------------
+
+# Deque implementation as python list, where the front of the deque is the end of the list; supported operations: isEmpty(), addFront(item), addRear(item), removeFront(), removeRear(), size()
+class Deque:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def addFront(self, item):
+        self.items.append(item)
+
+    def addRear(self, item):
+        self.items.insert(0,item)
+
+    def removeFront(self):
+        return self.items.pop()
+
+    def removeRear(self):
+        return self.items.pop(0)
+
+    def size(self):
+        return len(self.items)
