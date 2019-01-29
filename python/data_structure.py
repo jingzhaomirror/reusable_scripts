1e903afab91cc4f1e51a340fefa01fdf87106259
mirror <39384094+jingzhaomirror@users.noreply.github.com>
2019-01-28 22:05:53 -0800

    Update data_structure.py

--- python/data_structure.py
+++ python/data_structure.py
@@ -61,3 +61,146 @@ class Deque:
 
     def size(self):
         return len(self.items)
+# --------
+# unordered list implementation: The basic building block is the node holding at least two pieces of information: 1) its value/data 2) a reference to the next node. A reference to None will denote the fact that there is no next node.
+class Node:
+    def __init__(self,initdata):
+        self.data = initdata
+        self.next = None
+     
+    def getData(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    def setData(self,newdata):
+        self.data = newdata
+
+    def setNext(self,newnext):
+        self.next = newnext
+          
+class UnorderedList:
+    def __init__(self):
+        self.head = None
+          
+    def isEmpty(self):
+        return self.head == None
+     
+    def add(self,item):
+    # Since this list is unordered, the specific location of the new item with respect to the other items already in the list is not important
+    # it makes sense to place the new item in the easiest location possible which is the head of the list
+        temp = Node(item)
+        temp.setNext(self.head)
+        self.head = temp
+        
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count = count + 1
+            current = current.getNext()
+        return count
+
+    def search(self,item):
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.getData() == item:
+                found = True
+            else:
+                current = current.getNext()
+        return found
+
+    def remove(self,item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.getData() == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+        if previous == None:
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.getNext())
+    def append(self, item):
+     
+    def insert(self, position, item):
+    
+    def index(self, item):
+    
+    def pop(self, position=None):
+          
+# -------------
+# ordered list implementation: a collection of items where each item holds a relative position that is based upon some underlying characteristic of the item
+# The ordering is typically either ascending or descending and we assume that list items have a meaningful comparison operation that is already defined
+# Many of the ordered list operations are the same as those of the unordered list
+class OrderedList:
+    def __init__(self):
+        self.head = None
+          
+    def isEmpty(self):
+        return self.head == None
+     
+    def add(self,item):
+        current = self.head
+        previous = None
+        stop = False
+        while current != None and not stop:
+            if current.getData() > item:
+                stop = True
+            else:
+                previous = current
+                current = current.getNext()
+        temp = Node(item)
+        if previous == None:
+            temp.setNext(self.head)
+            self.head = temp
+        else:
+            temp.setNext(current)
+            previous.setNext(temp)
+          
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count = count + 1
+            current = current.getNext()
+        return count
+
+    def search(self,item):
+        current = self.head
+        found = False
+        stop = False
+        while current != None and not found and not stop:
+            if current.getData() == item:
+                found = True
+            elif current.getData() > item:
+                stop = True
+            else:
+                current = current.getNext()
+        return found
+
+    def remove(self,item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.getData() == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+        if previous == None:
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.getNext())
+               
+    def insert(self, position, item):
+    
+    def index(self, item):
+    
+    def pop(self, position=None):
