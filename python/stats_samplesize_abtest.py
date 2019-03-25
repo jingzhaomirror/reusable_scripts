fa32980d260172f5aae01b2c803cef37d1b2f127
mirror <39384094+jingzhaomirror@users.noreply.github.com>
2019-03-25 16:53:16 -0700

    Update and rename stats.py to stats_samplesize_abtest.py

--- python/stats_samplesize_abtest.py
+++ python/stats_samplesize_abtest.py
@@ -3,14 +3,14 @@
 @author: jingzhao
 """
 
-import math
 from scipy.stats import norm
+import math
 
 def find_z(alpha):
     """given significance level alpha, return a corresponding z value
     based on normal distribution, two-tailed"""
     
-    return norm.ppf(1-alpha/2)
+    return -norm.ppf(alpha/2)
 
 def find_beta(dmin, se_1, n, alpha):
     """
@@ -34,16 +34,24 @@ def find_samplesize(dmin, se_1, alpha=0.05, beta=0.2, ns=range(1,500000)):
     Inputs:
     d_min: The practical significance level
     se_1: The standard error of the metric with sample size n=1 in each group,
-            if given standard error at size n: se_1 = se_n * sqrt(n)
     alpha: The desired statistical significance level of the test
-    beta: The desired beta level (1-power) of the test
+    beta: The desired beta level (1-power or 1-sensitivity) of the test
     ns: a range of sample sizes to try out
+    
+    Calculating se_1:
+    For given empirical standard error at size n of each group: 
+        se_1 = se_n * sqrt(n)
+    For given real population p: 
+        the pooled standard error for N=1 in each group is:
+        se_1 = sqrt(p*(1-p)*(1/1 + 1/1))
    
     Returns: 
     The smallest n out of the given ns that will achieve the desired beta. 
     If none of the given Ns will work, returns -1.
-    note: This is the least required number of samples **in each group** of the experi
-ment. For AB testing, the minimium total sample size is 2*n. 
+    
+    note: 
+    This is the least required number of samples **in each group** of the experiment. 
+    For AB testing, the minimium total sample size is 2*n. 
     """
     
     for n in ns:
@@ -51,4 +59,3 @@ ment. For AB testing, the minimium total sample size is 2*n.
         if cur_beta <= beta:
             return n
     return -1
-    
